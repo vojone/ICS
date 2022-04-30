1171f824ccd0b59d9f6ccffa3b8e5bed95b54680
Tomáš Dvořák <xdvora3r@stud.fit.vutbr.cz>
2022-04-30 14:11:24 +0200

    Added missing BookRideControl.xaml file

---
+++ Carpool/Carpool.App/View/BookRideControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Carpool.App.View
+{
+    /// <summary>
+    /// Interaction logic for BookRideControl.xaml
+    /// </summary>
+    public partial class BookRideControl : UserControl
+    {
+        public BookRideControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxArrivalCreateRides_Copy1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEditRideSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
